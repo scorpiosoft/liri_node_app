7024a7a916fec40b1d8ff6885c733af27f7ee05a
Christopher Biessener <cpbiessener@gmail.com>
2018-02-02 17:37:42 -0600

    skeleton

---
+++ liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+// Add the code required to import the keys.js file and store it in a variable.
+  var spotify = new Spotify(keys.spotify);
+  var client = new Twitter(keys.twitter);
+
+// commands liri understands
+// * `my-tweets`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
